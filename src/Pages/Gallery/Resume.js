ccac4d7fa06e6fa803ca0f39f0256c77015d6567
daniel lanthier <daniellanthier2@gmail.com>
2022-04-12 09:23:29 -0400

    resume details

--- src/Pages/Gallery/Resume.js
+++ src/Pages/Gallery/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Grid, Box, Typography} from '@material-ui/core';
+import { Grid, Box, Typography,} from '@material-ui/core';
 import useStylesResume from './useStylesResume';
-import { CallOutlined, EmailOutlined, LocationOnOutlined, PermIdentityOutlined } from '@material-ui/icons';
+import { CallOutlined, EmailOutlined, LocationOnOutlined, PermIdentityOutlined, SchoolOutlined } from '@material-ui/icons';
 
 const Resume = () => {
   const classes = useStylesResume();
@@ -22,39 +22,100 @@ const Resume = () => {
         </Grid>
 
         <Grid item xs={6}>
-          <Typography variant="h3"> Contact </Typography>
+          <Typography variant="h4"> Contact </Typography>
           <Typography body="1"> <CallOutlined/> phone number</Typography>
           <Typography body="1"> <EmailOutlined/> email</Typography>
           <Typography body="1"> <LocationOnOutlined/> based in Stockholm, Sweden</Typography>
         </Grid>
 
         <Grid item xs={6}>
-          <Typography variant="h3"> <PermIdentityOutlined/> About me</Typography>
+          <Typography variant="h4"> <PermIdentityOutlined/> About me</Typography>
           <Typography body="1">A two-inch layer of freshly fallen snow covered the yard. Stacey peeked outside. To most, it would have been a beautiful sight worthy of taking a photo to put on Instagram. For Stacey, it meant something different. The first snow of the year brought back the witches as the fairies in the yard had to seek shelter to avoid the cold.</Typography>
         </Grid>
 
        <Grid item xs={6}>
-          <Typography body="1"> Soft Skills</Typography>
+          <Typography variant="h4"> Skills</Typography>
+
+         <div>
+          <Typography className={classes.Line} variant="h5">Speaking</Typography>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+         </div>
+
+         <div>
+          <Typography className={classes.Line} variant="h5">Walking</Typography>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+         </div>
+
+         <div>
+          <Typography className={classes.Line} variant="h5">Standing</Typography>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+         </div>
+         
         </Grid>
 
         <Grid item xs={6}>
-          <Typography body="1">work history</Typography>
+          <Typography variant="h4">work history</Typography>
+          <Typography variant="h5">2009 - 2013</Typography>
+          <Typography body="1">She was in a hurry. Not the standard hurry when you're in a rush to get someplace, but a frantic hurry. The type of hurry where a few seconds could mean life or death. She raced down the road ignoring speed limits and weaving between cars. She was only a few minutes away when traffic came to a dead standstill on the road ahead.</Typography>
         </Grid>
 
         <Grid item xs={6}>
-          <Typography body="1">Skills</Typography>
+          <Typography variant="h4">Soft Skills</Typography>
+         <div>
+          <Typography className={classes.Line} variant="h5">Speaking</Typography>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+         </div>
+
+         <div>
+          <Typography className={classes.Line} variant="h5">Walking</Typography>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+         </div>
+
+         <div>
+          <Typography className={classes.Line} variant="h5">Standing</Typography>
+           <span className={classes.Cdot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+           <span className={classes.Dot}></span>
+         </div>
         </Grid>
 
         <Grid item xs={6}>
-          <Typography body="1">work history</Typography>
+          <Typography variant="h4">work history</Typography>
+          <Typography variant="h5">2009 - 2013</Typography>
+          <Typography body="1">She was in a hurry. Not the standard hurry when you're in a rush to get someplace, but a frantic hurry. The type of hurry where a few seconds could mean life or death. She raced down the road ignoring speed limits and weaving between cars. She was only a few minutes away when traffic came to a dead standstill on the road ahead.</Typography>
         </Grid>
 
         <Grid item xs={6}>
-          <Typography body="1">education</Typography>
+          <Typography variant="h4"> <SchoolOutlined/> Education</Typography>
+          <Typography body="1">- A school name</Typography>
         </Grid>
 
        <Grid item xs={6}>
-          <Typography body="1">work history</Typography>
+          <Typography variant="h4">work history</Typography>
+          <Typography variant="h5">2009 - 2013</Typography>
+          <Typography body="1">She was in a hurry. Not the standard hurry when you're in a rush to get someplace, but a frantic hurry. The type of hurry where a few seconds could mean life or death. She raced down the road ignoring speed limits and weaving between cars. She was only a few minutes away when traffic came to a dead standstill on the road ahead.</Typography>
        </Grid>
 
       </Grid>
--- src/Pages/Gallery/useStylesResume.js
+++ src/Pages/Gallery/useStylesResume.js
@@ -3,16 +3,42 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => {
    return {
   Box:{
+    textAlign:'center',
     paddingTop: '10%',
+    padding:'5%',
     [theme.breakpoints.down('sm')]: {
             paddingTop: '25%',
+            padding:'0%',
         },
     overflow: 'hidden',
+},
+  Grid: {
+    overflowY:'scroll',
 },
   Underline:{
     borderBottom: '5px solid #2E2D29'
 },
-
+  Dot: {
+    borderRadius:'75%',
+    backgroundColor:'grey',
+    display:'inline-block',
+    height:'18px',
+    weight:'20px',
+    paddingLeft:'15px',
+    marginLeft:'5px',
+},
+  Cdot: {
+    borderRadius:'75%',
+    backgroundColor:'blue',
+    display:'inline-block',
+    height:'18px',
+    weight:'20px',
+    paddingLeft:'15px',
+    marginLeft:'5px',
+},
+Line: {
+display:'inline-block',
+},
 }    
 })
 
